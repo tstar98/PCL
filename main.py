086f7b55ae6043c7d16f6c12dd41253ab0dd3d85
Thomas <thomasstarnes98@gmail.com>
2021-12-10 13:10:55 -0700

    Changed model used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 
@@ -12,11 +12,10 @@ def labels2file(p, outf_path):
             outf.write('\t'.join([str(k) for k in pi]) + '\n')
 
 
-if __name__ == "__main__":
+def run():
     # get training data
-    df = pd.read_csv("pcl.tsv", sep='\t')
-
-    train, test = train_test_split(df, test_size=0.2)
+    train = pd.read_csv("pcl_train.tsv", sep='\t')
+    test = pd.read_csv("pcl_test.tsv", sep='\t')
 
     # vectorize text
     vectorizer = CountVectorizer(stop_words="english")
@@ -24,7 +23,8 @@ if __name__ == "__main__":
     y_train = [0 if label in (0, 1) else 1 for label in train["label"].values]
 
     # train the model
-    model = SGDClassifier()
+    print("Training...")
+    model = MLPClassifier(hidden_layer_sizes=(20,), solver='lbfgs')
     model.fit(x_train, y_train)
 
     # get testing data
@@ -32,15 +32,37 @@ if __name__ == "__main__":
     y_test = [0 if label in (0, 1) else 1 for label in test["label"].values]
 
     # testing
+    print("Testing...")
     y_pred = model.predict(x_test)
-    pred = []
-    #
-    # for i in range(len(y_pred)):
-    #     pred.append([df1["par_id"].values[i], art_id_test[i], df1["keyword"].values[i],
-    #                  df1["country_code"].values[i], df1["text"].values[i], y_pred[i]])
-    #
-    # labels2file(pred, "output.txt")
+
+    file = open("output", "a")
+    for i in range(len(y_pred)):
+        file.write(f"{test['par_id'].values[i]}\t{test['art_id'].values[i]}\t{test['keyword'].values[i]}"
+                   f"\t{test['country_code'].values[i]}\t\t{test['text'].values[i]}{y_pred[i]}\n")
 
     print(f"Precision: {precision_score(y_test, y_pred)}")
     print(f"Recall: {recall_score(y_test, y_pred)}")
     print(f"F score: {f1_score(y_test, y_pred)}")
+
+
+def split_data():
+    file = open("dev_set.csv")
+    par_id = {}
+    for line in file:
+        line = line.split(",")
+        par_id[line[0]] = True
+
+    file = open("pcl.tsv")
+    file1 = open("pcl_train.tsv", "a")
+    file2 = open("pcl_test.tsv", "a")
+
+    for line in file:
+        data = line.split('\t')
+        if data[0] in par_id:
+            file2.write(f"{line}")
+        else:
+            file1.write(f"{line}")
+
+
+if __name__ == "__main__":
+    run()
